fe31ee952f77a4984d1dd8bc2139f247763e5c35
nguyen cuong <vancuong.nguyen@savvycomsoftware.com>
2025-09-16 21:45:46 +0700

    Gradients in SwiftUI (SwiftUI Gradients, Linear, Radial, and Angular Gradients in SwiftUI)

--- LearningSwiftUI/LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+//            LinearGradient(
+//                stops: [
+//                    .init(color: .black, location: 0.2),
+//                    .init(color: .mint, location: 0.5)
+//                ],
+//                startPoint: .top,
+//                endPoint: .bottom
+//            )
+            
+//            RadialGradient(colors: [
+//                .black,
+//                .mint,
+//                .purple,
+//                .blue
+//            ], center: .top, startRadius: 50, endRadius: 100)
+            
+//            Text("Hello")
+//                .padding()
+//                .background(
+//                    AngularGradient(colors: [.red, .yellow, .green, .blue, .purple], center: .center, angle: .degrees(180))
+//                )
+            
+            AngularGradient(colors: [.red, .yellow, .green, .blue, .purple], center: .center, angle: .degrees(180))
+                .mask {
+                    Text("Hello")
+                        .bold()
+                        .font(.largeTitle)
+                }
         }
-        .padding()
     }
 }
 
