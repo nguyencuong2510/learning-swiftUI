0b4ca7a8044f6cebda77c09ced7b99301aeb47bf
nguyen cuong <vancuong.nguyen@savvycomsoftware.com>
2025-09-16 21:54:59 +0700

    Shapes in SwiftUI (SwiftUI Shapes, Shapes SwiftUI, SwiftUI How To Use Shapes)

--- LearningSwiftUI/LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -10,10 +10,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Capsule(style: .circular)
+                .fill(.blue)
+                .frame(width: 100, height: 50)
+            
+            Capsule(style: .continuous)
+                .frame(width: 100, height: 50)
+                
+            Circle()
+                .frame(width: 50, height: 50)
+            
+            Ellipse()
+                .frame(width: 50, height: 25)
+            
+            Rectangle()
+                .frame(width: 50, height: 50)
+                .cornerRadius(12)
+            
+            RoundedRectangle(cornerRadius: 12)
+                .frame(width: 50, height: 50)
+            
+            Image("logo")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
         }
         .padding()
     }
