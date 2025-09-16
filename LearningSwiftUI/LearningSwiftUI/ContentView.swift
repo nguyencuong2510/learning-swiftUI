58938f8db9c93f2690d0c3c6e18a413d32eb2122
nguyen cuong <vancuong.nguyen@savvycomsoftware.com>
2025-09-16 22:06:57 +0700

    How To Use Border in SwiftUI (SwiftUI Border, Border SwiftUI, Shape Border SwiftUI)

--- LearningSwiftUI/LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -10,10 +10,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//            Text("Hello, world!")
+//                .padding()
+//                .overlay {
+//                    RoundedRectangle(cornerRadius: 8)
+//                        .stroke(.blue, lineWidth: 5)
+//                }
+            
+            ZStack {
+                Circle()
+                    .strokeBorder(
+                        AngularGradient(
+                            gradient: Gradient(colors: [.red, .yellow, .red]),
+                            center: .center,
+                            endAngle: .degrees(360)
+                        ),
+                        lineWidth: 50
+                    )
+                    .padding()
+                
+                Text("100%")
+            }
+
+            
         }
         .padding()
     }
