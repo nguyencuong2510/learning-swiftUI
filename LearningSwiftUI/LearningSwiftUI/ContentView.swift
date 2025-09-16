656124b9f9764b2f81d5a8905e862dc98a452822
nguyen cuong <vancuong.nguyen@savvycomsoftware.com>
2025-09-16 22:26:08 +0700

    Layout, Frame, Alignment & Offset in SwiftUI (SwiftUI Frame, SwiftUI Alignment, SwiftUI Layout)

--- LearningSwiftUI/LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -9,13 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+//        VStack {
+//            Text("Hello, world!")
+//                .background(.blue)
+//
+//            Text("Hello, world!")
+//                .background(.blue)
+//
+//            Text("Hello, world!")
+//                .background(.blue)
+//        }
+//        .frame(
+//            maxWidth: .infinity,
+//            minHeight: 100,
+//            alignment: .topLeading
+//        )
+//        .background(.mint)
+//        .clipped()
+        
+        Image("logo")
+            .resizable()
+            .frame(width: 100, height: 100)
+            .clipShape(Circle())
+            .overlay(alignment: .bottomTrailing) {
+                Text("New Video")
+                    .foregroundColor(.white)
+                    .bold()
+                    .font(.callout)
+                    .padding(.vertical, 4)
+                    .padding(.horizontal, 8)
+                    .background(.red)
+                    .clipShape(Capsule())
+                    .offset(x: 25, y: 10)
+            }
+        
     }
 }
 
