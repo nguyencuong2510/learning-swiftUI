dcfba911ac00f719eea17fe08e99af341aa515f7
nguyen cuong <vancuong.nguyen@savvycomsoftware.com>
2025-09-22 23:16:37 +0700

    Divider in SwiftUI (SwiftUI Divider, SwiftUI Divider, Separate Content In SwiftUI with Divider)

--- LearningSwiftUI/LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -10,10 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Text("Hello, world! Nguyen van cuong")
+            
+            Divider()
+                .frame(width: 100)
+        }
+        .padding()
+        .background(.yellow)
+        
+        Group {
             Text("Hello, world!")
+            Divider()
+        }
+        .background(.mint)
+        
+        HStack {
+            Text("Hello, world!")
+            
+            Divider()
+                .frame(height: 50)
+            
+            Image(systemName: "lock")
         }
         .padding()
     }
