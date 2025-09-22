6dc0c5f14886033d9754a82134d70f5c6164724d
nguyen cuong <vancuong.nguyen@savvycomsoftware.com>
2025-09-22 23:27:35 +0700

    Color Picker in SwiftUI (SwiftUI Color Picker, ColorPicker SwiftUI, Color SwiftUI, SwiftUI Color)

--- LearningSwiftUI/LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var color: Color = .blue
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle()
+                .frame(width: 100, height: 100)
+                .foregroundStyle(color)
+                .overlay {
+                    Image(.logo)
+                        .padding()
+                        .foregroundStyle(.tint)
+                        .frame(width: 80, height: 80)
+                        .background(color)
+                        .clipShape(Circle())
+                }
+
+            ColorPicker(selection: $color, supportsOpacity: false) {
+                Label("Color Pallete", systemImage: "paintpalette")
+            }
         }
         .padding()
     }
