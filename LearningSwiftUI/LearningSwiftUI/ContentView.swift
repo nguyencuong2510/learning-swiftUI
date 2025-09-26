b123dbee026c74d8ec9ee121a5e6050b1bcf8158
cuong nguyen <>
2025-09-26 07:25:52 -0700

    How To Implement Padding SwiftUI (SwiftUI Padding, Padding SwiftUI, SwiftUI Padding Modifier)

--- LearningSwiftUI/LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -10,10 +10,39 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+//            Text("Hello, world!")
+//                .padding()
+//                .border(.red, width: 1)
+//            
+//            Text("Hello, world!")
+//                .padding(4)
+//                .border(.red, width: 1)
+//            
+//            Text("Hello, world!")
+//                .padding(.leading, 40)
+//                .border(.red, width: 1)
+//            
+//            Text("Hello, world!")
+//                .padding(.vertical, 40)
+//                .border(.red, width: 1)
+//                .padding(20)
+//                .border(.red, width: 1)
+            
+            VStack {
+                            Image(systemName: "globe")
+                                .imageScale(.large)
+                                .foregroundStyle(.tint)
+                
+                Text("Nguyen van cuogn tesst How To Implement Padding SwiftUI (SwiftUI Padding, Padding SwiftUI, SwiftUI Padding Modifier)")
+                    .frame(maxWidth: 200)
+                    .foregroundStyle(.white)
+                    .border(.red, width: 1)
+            }
+            .padding()
+            .background(.gray)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .border(.red, width: 1)
         }
         .padding()
     }
