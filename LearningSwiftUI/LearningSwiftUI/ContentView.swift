b731d83b2a481e6694632eccff77a52fbeac7b56
cuong nguyen <>
2025-09-26 08:51:04 -0700

    SwiftUI New Button Styles And Materials in SwiftUI (SwiftUI Button Styles, SwiftUI Materials)

--- LearningSwiftUI/LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -9,13 +9,89 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            VStack {
+                VStack {
+                    Text("Cuongnv button style")
+                    
+                    Button("Cuongnv", action: {
+                        print("cuon1")
+                    })
+                    .tint(.red)
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+                    .controlSize(.large)
+                    
+                    Button("Cuongnv", action: {
+                        print("cuon1")
+                    })
+                    .tint(.red)
+                    .buttonStyle(.bordered)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+                    .controlSize(.large)
+                    
+                    Button("Cuongnv", action: {
+                        print("cuon1")
+                    })
+                    .tint(.red)
+                    .buttonStyle(.borderless)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+                    .controlSize(.large)
+                    
+                    Button("Cuongnv", action: {
+                        print("cuon1")
+                    })
+                    .tint(.red)
+                    .buttonStyle(.plain)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+                    .controlSize(.large)
+                    
+                    
+                    Button("Cuongnv", action: {
+                        print("cuon1")
+                    })
+                    .tint(.red)
+                    .buttonStyle(.automatic)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+                    .controlSize(.large)
+                }
+                VStack {
+                    Text("Cuongnv Button Control Size")
+                    
+                    Button("Cuongnv", action: {
+                        print("cuon1")
+                    })
+                    .tint(.pink)
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+                    .controlSize(.mini)
+                    
+                    Button("Cuongnv", action: {
+                        print("cuon1")
+                    })
+                    .tint(.pink)
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+                    .controlSize(.small)
+                    
+                    Button("Cuongnv", action: {
+                        print("cuon1")
+                    })
+                    .tint(.pink)
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+                    .controlSize(.regular)
+                    
+                    Button("Cuongnv", action: {
+                        print("cuon1")
+                    })
+                    .tint(.pink)
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle(radius: 8))
+                    .controlSize(.large)
+                }
+            }
         }
-        .padding()
     }
 }
 
