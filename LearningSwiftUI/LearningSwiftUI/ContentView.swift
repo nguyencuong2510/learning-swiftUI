d48008a711e098dc68f8f875da536977987915cc
cuong nguyen <>
2025-09-26 08:18:09 -0700

    How To Use Stepper SwiftUI (SwiftUI Stepper, SwiftUI Plus Minus, Swift UI Stepper Example)

--- LearningSwiftUI/LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var qualityExampleOne: Int = 0
+    @State var qualityExampleTwo: Int = 0
+    @State var qualityExampleThree: Int = 0
+    @State var qualityExampleFour: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                Group {
+                    Text("Hello Every one \(qualityExampleOne)")
+                    Stepper("Quanlity", value: $qualityExampleOne, in: 0...20, step: 1)
+                }
+            }
+            
+            VStack {
+                Group {
+                    Text("Hello Every one \(qualityExampleTwo)")
+                    Stepper("", value: $qualityExampleTwo, in: 0...20, step: 1)
+                        .labelsHidden()
+                }
+            }
+            
+            VStack {
+                Group {
+                    
+                    Stepper(value: $qualityExampleThree, in: 0...20, step: 1) {
+                        Text("🤣 Slices \(qualityExampleThree)")
+                            .foregroundStyle(.black)
+                    }
+                }
+            }
+            
+            VStack {
+                Group {
+                    
+                    Stepper(value: $qualityExampleFour, in: 0...20, step: 1) {
+                        Text("🤣 Buggers \(qualityExampleFour)")
+                            .foregroundStyle(.red)
+                    } onEditingChanged: { editingStarted in
+                        print("value change \(editingStarted) quanlity is \(qualityExampleFour)")
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
