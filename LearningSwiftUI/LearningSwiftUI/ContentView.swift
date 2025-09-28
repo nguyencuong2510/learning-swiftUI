e0dc2450bf46c0cf24193aec4e12bdd7cfea44f0
cuong nguyen <>
2025-09-28 03:09:18 -0700

    Adding Text in SwiftUI And TextStyles (SwiftUI Text, SwiftUI TextStyle)

--- LearningSwiftUI/LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -10,12 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+                .bold()
+                .italic()
+                .underline(true, color: .red)
+            
+            Text("Hello, world!")
+                .foregroundStyle(.mint)
+                .font(.largeTitle.weight(.black))
+            
+            Text("Hello, world!")
+                .foregroundStyle(.blue)
+                .font(.system(size: 12, weight: .bold, design: .rounded))
+            
+            Text(.now, style: .timer)
+            
+            Text(.now, style: .time)
+            Text(.now, style: .date)
+            
+            Text(.now, format: Date.FormatStyle(date: .abbreviated, time: .shortened))
+            
+            Text(72.4, format: .currency(code: "GBP"))
+            Text(72.4, format: .currency(code: "USD"))
+            Text(Date.now...Date.now.addingTimeInterval(3600))
+            
+            Text("view calender \(Image(systemName: "calendar")) here")
         }
-        .padding()
+       
     }
 }
 
