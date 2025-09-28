58900daca607e6b56185536ec8d9e6a7d07bd0b1
cuong nguyen <>
2025-09-28 02:57:27 -0700

    Multi-Line TextField in SwiftUI with TextEditor (SwiftUI TextEditor, SwiftUI Multi-Line TextField)

--- LearningSwiftUI/LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var text: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            descriptionTxtView
+            textEditView
+            submitBtn
         }
         .padding()
+    }
+    
+    var textEditView: some View {
+        TextEditor(text: $text)
+            .frame(height: 250)
+            .overlay {
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(.gray.opacity(0.2), lineWidth: 4)
+            }
+    }
+    
+    var descriptionTxtView: some View {
+        Text("Description")
+    }
+    
+    var submitBtn: some View {
+        Button {
+            print("user tapped text \(text)")
+        } label: {
+            Text("Submit")
+        }
     }
 }
 
