c4e8590bd0bdd8c94583fc44d081e74873926bfa
cuong nguyen <>
2025-09-28 03:41:10 -0700

    Background and Overlay In SwiftUI (SwiftUI Background, SwiftUI Overlay)

--- LearningSwiftUI/LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -9,13 +9,114 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            VStack {
+                Text("5")
+                    .font(.footnote.weight(.bold))
+                    .padding()
+                    .background(.yellow)
+                
+                Text("5")
+                    .font(.footnote.weight(.bold))
+                    .padding()
+                    .background(content: {
+                        Image(systemName: "star")
+                            .symbolVariant(.fill)
+                            .foregroundStyle(.white)
+                            .font(.system(size: 32))
+                            .offset(y: -2)
+                    })
+                    .background(.yellow)
+                
+                Text("5")
+                    .font(.footnote.weight(.bold))
+                    .padding()
+                    .background(content: {
+                        
+                        Circle()
+                            .fill(.yellow.opacity(0.3))
+                        
+                        Image(systemName: "star")
+                            .symbolVariant(.fill)
+                            .foregroundStyle(.white)
+                            .font(.system(size: 32))
+                            .offset(y: -2)
+                    })
+                
+                Text("5")
+                    .font(.footnote.weight(.bold))
+                    .padding()
+                    .background(content: {
+                        Image(systemName: "star")
+                            .symbolVariant(.fill)
+                            .foregroundStyle(.green)
+                            .font(.system(size: 32))
+                            .offset(y: -2)
+                        
+                        Circle()
+                            .fill(.mint)
+                        
+                    })
+                
+                Text("5")
+                    .font(.footnote.weight(.bold))
+                    .padding()
+                    .background(alignment: .top, content: {
+                        Image(systemName: "star")
+                            .symbolVariant(.fill)
+                            .foregroundStyle(.green)
+                            .font(.system(size: 32))
+                            .offset(y: -2)
+                        
+                    })
+                
+                Text("5")
+                    .font(.footnote.weight(.bold))
+                    .frame(width: 100, height: 100)
+                    .background(content: {
+                        Image(systemName: "star")
+                            .symbolVariant(.fill)
+                            .foregroundStyle(.green)
+                            .font(.system(size: 32))
+                            .offset(y: -2)
+                        
+                        Circle()
+                            .fill(.mint)
+                        
+                    })
+                    .overlay(alignment: .bottom) {
+                        Text("Subcriber")
+                            .font(.footnote.weight(.bold))
+                            .foregroundStyle(.white)
+                            .padding(4)
+                            .background(Capsule()
+                                .fill(.red))
+                    }
+                
+                Text("5")
+                    .font(.footnote.weight(.bold))
+                    .frame(width: 100, height: 100)
+                    .background(content: {
+                        Image(systemName: "star")
+                            .symbolVariant(.fill)
+                            .foregroundStyle(.green)
+                            .font(.system(size: 32))
+                            .offset(y: -2)
+                        
+                        Circle()
+                            .fill(.mint)
+                        
+                    })
+                    .overlay(alignment: .bottom) {
+                        Text("Subcriber")
+                            .font(.footnote.weight(.bold))
+                            .foregroundStyle(.white)
+                            .padding(4)
+                            .background(.ultraThickMaterial, in: Capsule())
+                    }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
