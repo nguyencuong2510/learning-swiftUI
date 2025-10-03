c9024df01297f13d6fb421e5ddd24deff325437d
cuong nguyen <>
2025-10-03 10:01:51 -0700

    ScrollView in SwiftUI And Automatic Scrolling With ScrollViewReader In SwiftUI (SwiftUI ScrollView)

--- LearningSwiftUI/LearningSwiftUI/ContentView.swift
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var hasChangeValue = false
+    private let range: ClosedRange<Int> = 0...50
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollViewReader { proxy in
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(range, id: \.self) { item in
+                            createTxt(with: item)
+                        }
+                    }
+                    .padding(.bottom, 50)
+                    .overlay(alignment: hasChangeValue ? .bottomTrailing : .bottomLeading ) {
+                        HStack {
+                            Button {
+                                if let firstIndex = range.min() {
+                                    withAnimation {
+                                        proxy.scrollTo(firstIndex)
+                                    }
+                                    hasChangeValue = false
+                                }
+                            } label: {
+                                Image(systemName: "arrow.backward")
+                            }
+                            
+                            Button {
+                                if let lastIndex = range.max() {
+                                    withAnimation {
+                                        proxy.scrollTo(lastIndex)
+                                    }
+                                    hasChangeValue = true
+                                }
+                            } label: {
+                                Image(systemName: "arrow.forward")
+                            }
+                            
+                        }
+                        .symbolVariant(.circle.fill)
+                        .font(.title.weight(.black))
+                    }
+                }
+            }
         }
-        .padding()
+    }
+}
+
+private extension ContentView {
+    func createTxt(with index: Int) -> some View {
+        Text("\(index)")
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.mint, in: RoundedRectangle(cornerRadius: 8, style: .continuous))
+            .padding(.horizontal, 8)
     }
 }
 
